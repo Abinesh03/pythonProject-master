25ba24293079617777a9918518c565c9e229fca6
ananthan <Abineshau@gmail.com>
2023-01-28 23:58:56 +0530

    Update json file

---
+++ Edit json file.py
@@ -0,0 +1,37 @@
+import json
+
+
+def file_read():
+    with open("Names.json", "r+") as json_file:
+        data = json.load(json_file)
+        temp = data["names"]
+    return temp
+
+def file_write(json_object):
+    with open("Names1.json", "w") as f:
+        json.dump(json_object,f,indent=4)
+
+
+def task(temp):
+    y = {"name": "abi","age": 26}
+    temp.append(y)
+    return temp
+
+
+if __name__ == '__main__':
+    a = file_read()
+    b = task(temp=a)
+    file_write(json_object=b)
+
+
+# with open("Names.json", "r") as json_file:
+#     data = json.load(json_file)
+#     temp = data["names"]
+#     print(temp)
+#     print(type(temp))
+#     y = {"name":"Elango","age": 26}
+#     temp.append(y)
+#     print(temp)
+#     with open("Names1.json", "w") as f:
+#         json.dump(temp,f,indent=4)
+
--- Json.py
+++ Json.py
@@ -1,23 +1,23 @@
 import json
 
-# some JSON:
+#some JSON:
+
 #x =  '{ "name":"Abinesh", "age":27, "city":"Chennai"}'
 
-# parse x:
+#parse x:
 #y = json.loads(x)
 
-# the result is a Python dictionary:
+#the result is a Python dictionary:
 #print(y["name"])
 
-# a Python object (dict):
+#a Python object (dict):
 x = {
   "name": "Abinesh",
   "age": 27,
   "city": "Chennai"
 }
-
-# convert into JSON:
+#convert into JSON:
 y = json.dumps(x)
 
-# the result is a JSON string:
+#the result is a JSON string:
 print(y)
